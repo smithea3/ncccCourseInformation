3cf1f39390dc8ac87356417fd56c34d9ca0b4b0e
Ethan A. Smith <smithea3@outlook.com>
2023-05-09 21:01:45 -0400

    Update getInfo.py

--- getInfo.py
+++ getInfo.py
@@ -10,7 +10,7 @@ courseLabHours = coursesDB.get(subject).get(course).get('lab-hours')
 
 
 Element('course-title').write(courseTitle)
-Element('courseid-section-semester').write(courseTitle+"-"section+semester)
+Element('courseid-section-semester').write(courseTitle+"-"+section+semester)
 Element('credit-hours').element.append(courseCreditHours)
 Element('class-hours').element.append(courseClassHours)
 Element('lab-hours').element.append(courseLabHours)
