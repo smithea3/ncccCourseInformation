8c75050b6dea88d005f63372c004881925ab0697
Ethan A. Smith <smithea3@outlook.com>
2023-05-09 20:43:25 -0400

    Update getInfo.py

--- getInfo.py
+++ getInfo.py
@@ -1,11 +1,12 @@
 import json
-subject = {pyscript.interpreter.globals.get('subject')}
-course = {pyscript.interpreter.globals.get('course')}
-section = {pyscript.interpreter.globals.get('section')}
-school = {pyscript.interpreter.globals.get('school')}
-fp = open("courses.json")
+subject = pyscript.interpreter.globals.get('subject')
+course = pyscript.interpreter.globals.get('course')
+section = pyscript.interpreter.globals.get('section')
+school = pyscript.interpreter.globals.get('school')
+
+fp = open('courses.json')
 coursesDB = json.load(fp)
-Element("course-title").write(coursesDB.get(subject).get(course).get("title"))
-Element("credit-hours").element.append(coursesDB.get(subject).get(course).get("credit-hours"))
-Element("class-hours").element.append(coursesDB.get(subject).get(course).get("class-hours"))
-Element("lab-hours").element.append(coursesDB.get(subject).get(course).get("lab-hours"))
+Element('course-title').write(coursesDB.get(subject).get(course).get('title'))
+Element('credit-hours').element.append(coursesDB.get(subject).get(course).get('credit-hours'))
+Element('class-hours').element.append(coursesDB.get(subject).get(course).get('class-hours'))
+Element('lab-hours').element.append(coursesDB.get(subject).get(course).get('lab-hours'))
