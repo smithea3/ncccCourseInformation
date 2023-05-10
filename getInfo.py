5685bece2af0b2c859a25f94499ebcfbe4f64cde
Ethan A. Smith <smithea3@outlook.com>
2023-05-09 21:09:27 -0400

    Update infromation

--- getInfo.py
+++ getInfo.py
@@ -4,13 +4,14 @@ fp = open('courses.json')
 coursesDB = json.load(fp)
 
 courseTitle = coursesDB.get(subject).get(course).get('title')
+courseTitleComplete = courseTitle+"-"+section+semester
 courseCreditHours = coursesDB.get(subject).get(course).get('credit-hours')
 courseClassHours = coursesDB.get(subject).get(course).get('class-hours')
 courseLabHours = coursesDB.get(subject).get(course).get('lab-hours')
 
 
 Element('course-title').write(courseTitle)
-Element('courseid-section-semester').write(courseTitle,"-",section,semester)
+Element('courseid-section-semester').write(courseTitleComplete)
 Element('credit-hours').element.append(courseCreditHours)
 Element('class-hours').element.append(courseClassHours)
 Element('lab-hours').element.append(courseLabHours)
