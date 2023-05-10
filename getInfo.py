7d53b1bb94ab1e351a1d92ab5d668e97cf8a6c38
Ethan A. Smith <smithea3@outlook.com>
2023-05-09 20:55:39 -0400

    Update getInfo.py

--- getInfo.py
+++ getInfo.py
@@ -1,8 +1,16 @@
 import json
-from js import subject, course, section    
+from js import subject, course, section, semester    
 fp = open('courses.json')
 coursesDB = json.load(fp)
-Element('course-title').write(coursesDB.get(subject).get(course).get('title'))
-Element('credit-hours').element.append(coursesDB.get(subject).get(course).get('credit-hours'))
-Element('class-hours').element.append(coursesDB.get(subject).get(course).get('class-hours'))
-Element('lab-hours').element.append(coursesDB.get(subject).get(course).get('lab-hours'))
+
+courseTitle = coursesDB.get(subject).get(course).get('title')
+courseCreditHours = coursesDB.get(subject).get(course).get('credit-hours')
+courseClassHours = coursesDB.get(subject).get(course).get('class-hours')
+courseLabHours = coursesDB.get(subject).get(course).get('lab-hours')
+
+
+Element('course-title').write(courseTitle)
+Element('courseid-section-semester').write("{courseTitle}-{section} {Semester}")
+Element('credit-hours').element.append(courseCreditHours)
+Element('class-hours').element.append(courseClassHours)
+Element('lab-hours').element.append(courseLabHours)
