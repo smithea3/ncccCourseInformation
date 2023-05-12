876bbdaa11a026a5d3ee3b83d671ed5b83b9e593
Ethan A. Smith <smithea3@outlook.com>
2023-05-12 09:44:40 -0400

    Update getInfo.py and index.html

--- getInfo.py
+++ getInfo.py
@@ -9,6 +9,7 @@ courseCreditHours = coursesDB.get(subject).get(course).get('credit-hours')
 courseClassHours = coursesDB.get(subject).get(course).get('class-hours')
 courseLabHours = coursesDB.get(subject).get(course).get('lab-hours')
 catalogDescription = coursesDB.get(subject).get(course).get('catalog-description')
+courseSLOs = coursesDB.get(subject).get(course).get('slos')
 
 
 Element('course-title').write(courseTitle)
@@ -17,3 +18,5 @@ Element('credit-hours').element.append(courseCreditHours)
 Element('class-hours').element.append(courseClassHours)
 Element('lab-hours').element.append(courseLabHours)
 Element('catalog-description').write(catalogDescription)
+for slo in courseSLOs:
+    Element('slo-list').element.append("<li>{slo}</li>")
