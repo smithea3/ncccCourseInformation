47d87c3e58b3e4b7c6bee2d0d09cdd126f38fdbd
Ethan A. Smith <smithea3@outlook.com>
2023-05-14 13:18:18 -0400

    Update college_policies and getInfo
    Fixed grammar and spelling in policies and removed python terminal printout in getInfo.py

--- getInfo.py
+++ getInfo.py
@@ -53,6 +53,7 @@ Element('catalog-description').write(catalogDescription)
 ### Calls the helper function createList() to to add the HTML to the slo-list div.
 Element('slo-list').element.innerHTML = createList(courseSLOs)
 
+### Gets college policies and prints them at the end of the syllabus
 Element('college-policies').element.innerHTML = getCollegePolicies(collegePoliciesDB, college)
 
 
