c4207cadbc6a4416a70074ab13ad59b811186c23
Ethan A. Smith <smithea3@outlook.com>
2023-05-14 13:07:52 -0400

    Update getInfo.py

--- getInfo.py
+++ getInfo.py
@@ -21,6 +21,7 @@ def getCollegePolicies(policiesDB, college):
         content = collegePolicies.get(policy).get('content')
         output += f"<h1>{title}</h1>"
         output += content
+    print(output)
     return output
 
 collegeName = collegePoliciesDB.get(college).get('collegeName')
