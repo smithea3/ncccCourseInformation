c3dc0d204cc65f1bf6499bb6f8f26d07248bee97
Ethan A. Smith <smithea3@outlook.com>
2023-05-14 12:29:20 -0400

    Updates to policies json and getInfo.py

--- getInfo.py
+++ getInfo.py
@@ -16,6 +16,28 @@ courseLabHours = coursesDB.get(subject).get(course).get('lab-hours')
 catalogDescription = coursesDB.get(subject).get(course).get('catalog-description')
 courseSLOs = coursesDB.get(subject).get(course).get('slos')
 
+### Writes the college's name into the 'collegeName' ID
+Element('college-name').write(collegeName)
+
+### Writes the course tite
+Element('course-title').write(courseTitle)
+
+### Writes the course information in the format AAA-###-AAAAA YYYYAA
+Element('courseid-section-semester').write(courseTitleComplete)
+
+### Appends the credit hours, class hours, and lab hours to their respective places
+Element('credit-hours').element.append(courseCreditHours)
+Element('class-hours').element.append(courseClassHours)
+Element('lab-hours').element.append(courseLabHours)
+
+### Writes the course catalog description to its respective place
+Element('catalog-description').write(catalogDescription)
+
+### Calls the helper function createList() to to add the HTML to the slo-list div.
+Element('slo-list').element.innerHTML = createList(courseSLOs)
+
+Element('college-policies').element.innerHTML = createList(getCollegePolicies(collegePoliciesDB))
+
 
 def createList(x):
     output = "<ul>"
@@ -24,12 +46,12 @@ def createList(x):
     output += "</ul>"
     return output
 
-Element('course-title').write(courseTitle)
-Element('courseid-section-semester').write(courseTitleComplete)
-Element('credit-hours').element.append(courseCreditHours)
-Element('class-hours').element.append(courseClassHours)
-Element('lab-hours').element.append(courseLabHours)
-Element('catalog-description').write(catalogDescription)
-Element('slo-list').element.innerHTML = createList(courseSLOs)
-Element('college-name').write(collegeName)
-
+def getCollegePolicies(policiesDB):
+    collegePolicies = policiesDB.get('policies')
+    output = ""
+    for policy in policiesDB:
+        title = policies.get('title')
+        content = policies.get('content')
+        output += f"<h1>{title}</h1>"
+        output += content
+    return output
