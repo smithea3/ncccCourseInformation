daa9a570ff77bdb7bb578445c4f980ac0bd8bd42
Ethan A. Smith <smithea3@outlook.com>
2023-07-03 20:17:58 -0400

    Refactored the function to get url params

--- js/get_info.js
+++ js/get_info.js
@@ -37,66 +37,17 @@ function convertArrayToHtmlList (inputList, listType) {
     return outputHTML
 }
 
-// A function go retrieve all of the URL Paramters take from https://www.sitepoint.com/get-url-parameters-with-javascript/
-function getAllUrlParams(url) {
-    // get query string from url (optional) or window
-    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-    // we'll store the parameters here
-    var obj = {};
-
-    // if query string exists
-    if (queryString) {
-
-        // stuff after # is not part of query string, so get rid of it
-        queryString = queryString.split('#')[0];
-
-        // split our query string into its component parts
-        var arr = queryString.split('&');
-
-        for (var i = 0; i < arr.length; i++) {
-            // separate the keys and the values
-            var a = arr[i].split('=');
-
-            // set parameter name and value (use 'true' if empty)
-            var paramName = a[0];
-            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-
-            // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-            if (paramName.match(/\[(\d+)?\]$/)) {
-
-                // create key if it doesn't exist
-                var key = paramName.replace(/\[(\d+)?\]/, '');
-                if (!obj[key]) obj[key] = [];
-
-                // if it's an indexed array e.g. colors[2]
-                if (paramName.match(/\[\d+\]$/)) {
-                    // get the index value and add the entry at the appropriate position
-                    var index = /\[(\d+)\]/.exec(paramName)[1];
-                    obj[key][index] = paramValue;
-                } else {
-                    // otherwise add the value to the end of the array
-                    obj[key].push(paramValue);
-                }
-            } else {
-                // we're dealing with a string
-                if (!obj[paramName]) {
-                    // if it doesn't exist, create property
-                    obj[paramName] = paramValue;
-                } else if (obj[paramName] && typeof obj[paramName] === 'string') {
-                    // if property does exist and it's a string, convert it to an array
-                    obj[paramName] = [obj[paramName]];
-                    obj[paramName].push(paramValue);
-                } else {
-                    // otherwise add the property
-                    obj[paramName].push(paramValue);
-                }
-            }
-        }
+// Function to get URL Parameters (updated)
+function getURLParameters(url) {
+    const params = {};
+    const urlParams = new URLSearchParams(url);
+  
+    for (let [key, value] of urlParams) {
+      params[key] = value;
     }
-
-    return obj;
-}
+  
+    return params;
+  }
 
 function getCourseInformation(urlParams) {
     // Get the college name
@@ -166,6 +117,6 @@ function getInstructorInformation(urlParams) {
     }
 }
 
-var urlParams = getAllUrlParams()
+var urlParams = getURLParameters(window.location.search)
 getCourseInformation(urlParams)
 getInstructorInformation(urlParams)
